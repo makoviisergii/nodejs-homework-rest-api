e6b05026cd46bd0b72f41045dbfff2b52815ebc9
makoviisergii <sergiimakovii78@gmail.com>
2023-05-20 15:25:12 +0300

    20.05 15.00

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://SergiiM:3e59LNicwx5h43KK@cluster0.c7tf8b3.mongodb.net/";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
